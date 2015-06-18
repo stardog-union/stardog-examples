d47a6bf45a2a7d249760b26669af11271df1aab3
Evren Sirin <evren@clarkparsia.com>
2015-06-18 16:25:34 -0400

    Add readme file for FOAF archetype example

--- examples/foaf/main/src/com/complexible/stardog/foaf/FOAFArchetype.java
+++ examples/foaf/main/src/com/complexible/stardog/foaf/FOAFArchetype.java
@@ -15,7 +15,6 @@
 
 package com.complexible.stardog.foaf;
 
-import com.complexible.stardog.db.DatabaseArchetype;
 import org.openrdf.model.vocabulary.FOAF;
 
 import com.complexible.common.rdf.model.Values;
@@ -23,16 +22,18 @@ import com.complexible.stardog.icv.ConstrainedDatabaseArchetypeBase;
 
 /**
  * User-defined database archetype example for FOAF. This example shows an archetype with constraints but we can
- * also define archetypes without constraints by extending {@link DatabaseArchetype} instead.
+ * also define archetypes without constraints by extending {@code com.complexible.stardog.db.DatabaseArchetypeBase}
+ * instead. An archetype implementation is typically empty when one of the base classes are extended and simply
+ * provides the name, namespace, ontologies, and constraints by calling the super class constructor.
  * 
  * @author Evren Sirin
  */
 public class FOAFArchetype extends ConstrainedDatabaseArchetypeBase {
 	public FOAFArchetype() {
-	    super(FOAF.PREFIX, // name of the archetype (also used as the prefix in namespaces)
-	          Values.uri(FOAF.NAMESPACE), // namespace for the archetype (used in stored namesapces)
-	          readStatements(FOAFArchetype.class, "foaf.rdf"), // one or more ontologies for the archetype
-	          readStatements(FOAFArchetype.class, "foaf-constraints.ttl")
+	    super("foaf", // name of the archetype (used in database options and also as the prefix for the namespaces)
+	          Values.uri(FOAF.NAMESPACE), // namespace for the archetype (used in stored namespaces)
+	          readStatements(FOAFArchetype.class, "foaf.rdf"), // one or more ontology files
+	          readStatements(FOAFArchetype.class, "foaf-constraints.ttl") // one or more constraint files
 	    );
     }
 }
