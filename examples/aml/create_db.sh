5b84cdffba3651ef00e7b766f84b6f09735c7ccd
Paul Jackson <paul.jackson@stardog.com>
2018-04-12 16:01:12 -0400

    Clean up, add note on reasoning

--- examples/aml/create_db.sh
+++ examples/aml/create_db.sh
@@ -13,7 +13,7 @@ stardog-admin virtual import $DB bank-account-mapping.ttl aml_dataset_bank_accou
 stardog-admin virtual import $DB company-mapping.ttl aml_dataset_companies.csv
 stardog-admin virtual import $DB people-mapping.ttl aml_dataset_people.csv
 
-# These files allow multiple people and companies to share an address
+# These files allow multiple people and companies to share addresses
 stardog-admin virtual import $DB company_address-mapping.ttl aml_dataset_companies_addresses.csv
 stardog-admin virtual import $DB people_address-mapping.ttl aml_dataset_people_addresses.csv
 
@@ -27,7 +27,7 @@ stardog-admin virtual import $DB people_shares-mapping.ttl aml_dataset_people_sh
 stardog-admin virtual import $DB transaction-mapping.ttl aml_dataset_transactions.csv
 
 # Or, comment the prior line and uncomment these lines to load the transactions into MySQL
-# Alterntively, can use load_transactions.sql instead of load_transactions-mysql.sql
+# Alternatively, can use load_transactions.sql instead of load_transactions-mysql.sql
 #mysql -uadmin -padmin -hlocalhost stardog < load_transactions-mysql.sql
 #stardog-admin virtual add aml.properties transaction-mapping-sql.ttl
 
