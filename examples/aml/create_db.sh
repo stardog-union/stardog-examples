78c0ac70eb8e98af25260bc9b822efa44e6b3bba
Paul Jackson <paul.jackson@stardog.com>
2018-04-16 10:45:55 -0400

    Replace bnodes with real IRIs

--- examples/aml/create_db.sh
+++ examples/aml/create_db.sh
@@ -6,7 +6,8 @@ DB=fraud_demo
 
 stardog-admin db drop $DB
 
-stardog-admin db create -o spatial.enabled=true -n $DB namespaces.ttl
+stardog-admin db create -o spatial.enabled=true -o search.enabled=true -n $DB
+stardog namespace add $DB --prefix "" --uri tag:stardog:demo:aml:
 
 stardog-admin virtual import $DB address-mapping.ttl aml_dataset_addresses.csv
 stardog-admin virtual import $DB bank-account-mapping.ttl aml_dataset_bank_accounts.csv
