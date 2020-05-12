498e185b46bf58af013088d34c7beaf9856cfef0
Joe Hayes <joseph.hayes@stardog.com>
2020-05-12 15:08:07 -0400

    Adds dotNetRDF and .NET HTTP API examples to dotnet folder

---
+++ examples/dotnet/DotNetRDFConsoleSample/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Storage;
+using VDS.RDF.Storage.Management;
+using VDS.RDF.Storage.Management.Provisioning;
+
+namespace DotNetRDFConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will print out basic traces of HTTP requests and responses to the Console 
+            Options.HttpDebugging = true;
+
+            const string SERVER_URL = "http://localhost:5820";
+            const string STARDOG_USERNAME = "admin";
+            const string STARDOG_PASSWORD = "admin";
+            const string DATABASE_NAME = "MyDB";
+
+            // using will automatically call Dispose() for us
+            using (StardogServer stardog = new StardogServer(SERVER_URL, STARDOG_USERNAME, STARDOG_PASSWORD))
+            {
+
+                // if the database already exists, delete it
+                if (stardog.ListStores().Contains(DATABASE_NAME))
+                {
+                    stardog.DeleteStore(DATABASE_NAME);
+                }
+
+                IStoreTemplate template = stardog.GetDefaultTemplate(DATABASE_NAME);
+                Console.WriteLine("Template ID " + template.ID);
+
+                stardog.CreateStore(template);
+            }
+
+            using (StardogConnector stardogConn = new StardogConnector(SERVER_URL, DATABASE_NAME, STARDOG_USERNAME, STARDOG_PASSWORD))
+            {
+                // Construct the Triple we wish to add
+                Graph g = new Graph();
+                INode s = g.CreateBlankNode();
+                INode p = g.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+                INode o = g.CreateUriNode(new Uri("http://example.org/Example"));
+                Triple t = new Triple(s, p, o);
+
+                // Now add a Triple to a Graph in the Store
+                if (stardogConn.UpdateSupported)
+                {
+                    // UpdateGraph takes enumerables of Triples to add/remove or null to indicate none
+                    // Hence why we create a Triple array to pass in the Triple to be added
+                    stardogConn.UpdateGraph("http://example.org/graph", new Triple[] { t }, null);
+                }
+                else
+                {
+                    throw new Exception("Store does not support triple level updates");
+                }
+
+                // OR load from a file:
+
+                //Graph g = new Graph();
+                //g.LoadFromFile("example.rdf");
+
+                //// Set its BaseUri property to the URI we want to save it as
+                //g.BaseUri = new Uri("http://example.org/graph");
+
+                //// Now save it to the store
+                //if (!stardogConn.IsReadOnly)
+                //{
+                //    stardogConn.SaveGraph(g);
+                //}
+
+                // Make a SPARQL Query against the store
+                object results = stardogConn.Query("SELECT * WHERE { GRAPH ?g { ?s ?p ?o } } LIMIT 100");
+                if (results is SparqlResultSet)
+                {
+                    //Print the results
+                    SparqlResultSet rset = (SparqlResultSet)results;
+                    foreach (SparqlResult r in rset)
+                    {
+                        Console.WriteLine("RESULT: {0}", r.ToString());
+                    }
+                }
+                else
+                {
+                    throw new Exception("Did not get a SPARQL Result Set as expected");
+                }
+
+                // List the Graphs
+                // Making sure that we check this feature is supported first
+                if (stardogConn.ListGraphsSupported)
+                {
+                    //Iterate over the Graph URIs and print them
+                    foreach (Uri u in stardogConn.ListGraphs())
+                    {
+                        Console.WriteLine("GRAPH: {0}", u.ToString());
+                    }
+                }
+                else
+                {
+                    throw new Exception("Store does not support listing graphs");
+                }
+
+                // Now delete the Triple we added earlier in the Store
+                if (stardogConn.UpdateSupported)
+                {
+                    stardogConn.UpdateGraph("http://example.org/graph", null, new Triple[] { t });
+                }
+                else
+                {
+                    throw new Exception("Store does not support triple level updates");
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
+    
---
+++ examples/dotnet/HttpApiConsoleSample/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HttpApiConsoleSample
+{
+    public class DatabaseList
+    {
+        [JsonProperty("databases")]
+        public List<string> Databases { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SERVER_URL = "http://localhost:5820/";
+            const string STARDOG_USERNAME = "admin";
+            const string STARDOG_PASSWORD = "admin";
+            const string DATABASE_NAME = "MyDB";
+
+            Uri baseServerUri = new Uri(SERVER_URL);
+            CredentialCache requestCredentialsCache = new CredentialCache();
+            requestCredentialsCache.Add(baseServerUri, "Basic", new NetworkCredential(STARDOG_USERNAME, STARDOG_PASSWORD));
+
+            // Get a list of all the server's databases
+
+            HttpWebRequest listDatabasesRequest = (HttpWebRequest)WebRequest.Create(new Uri(baseServerUri, "admin/databases"));
+
+            listDatabasesRequest.ContentType = "application/json";
+            listDatabasesRequest.Credentials = requestCredentialsCache;
+            listDatabasesRequest.Method = "GET";
+
+            using (HttpWebResponse listDatabasesResponse = (HttpWebResponse)listDatabasesRequest.GetResponse())
+            {
+                if (listDatabasesResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var sr = new StreamReader(listDatabasesResponse.GetResponseStream()))
+                    {
+                        var databasesAsJson = sr.ReadToEnd();
+                        var databases = JsonConvert.DeserializeObject<DatabaseList>(databasesAsJson);
+                        if (databases.Databases.Contains(DATABASE_NAME))
+                        {
+                            // Database exists, DROP it
+
+                            HttpWebRequest deleteDatabaseRequest = (HttpWebRequest)WebRequest.Create(new Uri(baseServerUri, $"admin/databases/{DATABASE_NAME}"));
+                            deleteDatabaseRequest.Method = "DELETE";
+                            deleteDatabaseRequest.Credentials = requestCredentialsCache;
+                            using (HttpWebResponse deleteDatabaseResponse = (HttpWebResponse)deleteDatabaseRequest.GetResponse())
+                            {
+                                if (deleteDatabaseResponse.StatusCode == HttpStatusCode.OK)
+                                {
+                                    Console.WriteLine($"Database {DATABASE_NAME} deleted!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"ERROR! {deleteDatabaseResponse.StatusDescription}");
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            // Create the database
+
+            HttpWebRequest createDatabasesRequest = (HttpWebRequest)WebRequest.Create(new Uri(baseServerUri, "admin/databases"));
+
+            createDatabasesRequest.Credentials = requestCredentialsCache;
+            createDatabasesRequest.Method = "POST";
+            createDatabasesRequest.Accept = "*/*";
+            createDatabasesRequest.Headers.Add("Cache-Control", "no-cache");
+            createDatabasesRequest.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+            createDatabasesRequest.UserAgent = "Stardog DotNet Sample";
+
+            
+            string boundary = "--------------------------" + DateTime.Now.Ticks.ToString("x", NumberFormatInfo.InvariantInfo); ;
+
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+            byte[] terminatorBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            createDatabasesRequest.ContentType = "multipart/form-data;; boundary=" + boundary;
+
+            JObject json = new JObject();
+            json.Add("dbname", new JValue(DATABASE_NAME));
+            json.Add("options", new JObject());
+
+            JObject file = new JObject();
+            file.Add("filename", new JValue("fileX.ttl"));
+
+            json.Add("files", new JArray() { file });
+
+            using (Stream s = createDatabasesRequest.GetRequestStream())
+            {
+                // Boundary
+                s.Write(boundaryBytes, 0, boundaryBytes.Length);
+
+                // Root Item
+                string rootItem = string.Format("Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}", "root", json.ToString());
+                byte[] itemBytes = Encoding.UTF8.GetBytes(rootItem);
+                s.Write(itemBytes, 0, itemBytes.Length);
+
+                // Boundary
+                s.Write(boundaryBytes, 0, boundaryBytes.Length);
+
+                // File Item
+                string fileItem = string.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n", "fileX.ttl", "fileX.ttl");
+                fileItem += "Content-Type: text/turtle";
+                fileItem += "\r\n\r\n";
+                fileItem += "<urn:a> <urn:b> <urn:c> .";
+                fileItem += "\r\n";
+
+                byte[] fileBytes = Encoding.UTF8.GetBytes(fileItem);
+                s.Write(fileBytes, 0, fileBytes.Length);
+
+                // Then terminating boundary
+                s.Write(terminatorBytes, 0, terminatorBytes.Length);
+                s.Close();
+            }
+
+            using (HttpWebResponse createDatabasesResponse = (HttpWebResponse)createDatabasesRequest.GetResponse())
+            {
+                using (var sr = new StreamReader(createDatabasesResponse.GetResponseStream()))
+                {
+                    var responseText = sr.ReadToEnd();
+                    Console.WriteLine(responseText);
+                }
+            }
+        }
+    }
+}
