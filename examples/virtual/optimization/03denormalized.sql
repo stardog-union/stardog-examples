78db4bb74acd64cf7839205cd0f6f0269ae395a6
Paul Jackson <pauljackson123@verizon.net>
2026-03-17 14:13:39 -0400

    Updates for v12

--- examples/virtual/optimization/03denormalized.sql
+++ examples/virtual/optimization/03denormalized.sql
@@ -13,3 +13,5 @@ CREATE TABLE Roles(movie_id  INTEGER     NOT NULL,
 
 INSERT INTO Roles VALUES(1, 'The Princess Bride', 1987, 1, 'Robin Wright',      'Buttercup');
 INSERT INTO Roles VALUES(2, 'Unbreakable',        2000, 1, 'Robin Wright Penn', 'Audrey Dunn');
+INSERT INTO Roles VALUES(3, 'Apocalypse Now',     1979, 3, 'Larry Fishburne',   'Mr. Clean');
+INSERT INTO Roles VALUES(4, 'The Matrix',         1999, 3, 'Laurence Fishburne','Morpheus');
